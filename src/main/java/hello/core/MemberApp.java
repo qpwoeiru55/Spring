cc4a29cd60b790c9619a2c50231581935de2aefb
DESKTOP-1EDMQ3M\82104 <qpwoeiru55@naver.com>
2025-06-19 14:02:55 +0900

    2일차

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,13 +3,18 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
-import org.springframework.cglib.core.VisibilityPredicate;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class MemberApp {
+public class  MemberApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member membera = new Member(1L, "membera", Grade.vip);
 
         memberService.join(membera);
